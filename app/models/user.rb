e27b2c582c96ef2ac90e634b801aba8dddcec199
Ricardo Nacif <ricardonacif@lancedavez.com.br>
2014-07-25 22:43:13 -0300

    add devise and rspec

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable and :omniauthable
+  devise  :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
